f0510b4426ce4f59869079ae8121582f94565ddf
DavidLiubchenko <sharingan18091997@gmail.com>
2021-11-03 13:28:04 +0200

    start_work_autosize_hmPage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <h2 id="hmPage">Home Page</h2>
   <div id="carusel">
-    <button @click="toTheLeft"  :class="{ unVisible: position == 0 }">
-      <img src="../imgs/left_click.png" alt="leftClickButt" class="arrow prev">
+    <button @click="toTheLeft" :class="{ unVisible: position == 0 }">
+      <img src="../imgs/left_click.png" alt="leftClickButt" class="arrow prev" />
     </button>
     <div class="allPosts">
       <div
@@ -17,8 +17,8 @@
       </div>
     </div>
 
-    <button v-if="position != 5" @click="toTheRight" >
-      <img src="../imgs/right_click.png" alt="RightClickButt" class="arrow next">
+    <button v-if="position != 5" @click="toTheRight">
+      <img src="../imgs/right_click.png" alt="RightClickButt" class="arrow next" />
     </button>
   </div>
 </template>
@@ -61,7 +61,13 @@ export default {
           id: i,
           title: Data.films[i].nameEn,
           imgSrc: Data.films[i].posterUrl,
-          body: "Year: "+Data.films[i].year+", Rating: "+Data.films[i].rating+", Country: "+Data.films[i].countries[0].country,
+          body:
+            "Year: " +
+            Data.films[i].year +
+            ", Rating: " +
+            Data.films[i].rating +
+            ", Country: " +
+            Data.films[i].countries[0].country,
         };
         this.posts.push(NewPost);
       }
@@ -79,5 +85,78 @@ export default {
 
 <style scoped>
 @import "./styleHome.css";
+@import url("https://fonts.googleapis.com/css2?family=Raleway:wght@800&display=swap");
 
+h4,
+h5 {
+  margin: 15px;
+  font-family: "Raleway", sans-serif;
+  text-align: center;
+}
+h4 {
+  color: rgb(131, 131, 131);
+}
+#hmPage {
+  margin-top: 0;
+  font-size: 30px;
+  font-family: "Raleway", sans-serif;
+  position: relative;
+  top: 8%;
+  left: 17.5%;
+}
+
+#carusel {
+  position: relative;
+  padding: 20px;
+  margin: 100px 15%;
+  height: 500px;
+  width: 1100px;
+  overflow: hidden;
+}
+.allPosts {
+  width: 9999px;
+  height: 440px;
+}
+.post {
+  position: relative;
+  display: inline-block;
+  border: 4px solid #ebebeb;
+  margin: 0 20px;
+  height: 440px;
+  width: 320px;
+}
+.srcImg {
+  margin: 5px;
+  display: block;
+  width: auto;
+  height: 350px;
+  text-align: center;
+  padding-left: 35px;
+  margin-bottom: 20px;
+}
+.unVisible {
+  visibility: hidden;
+}
+.prev {
+  left: 20px;
+}
+
+.next {
+  right: 5px;
+}
+.arrow {
+  z-index: 1;
+  position: absolute;
+  top: 48%;
+  padding: 0;
+  background: white;
+  border-radius: 20px;
+  font-size: 24px;
+  line-height: 24px;
+  color: #444;
+  display: table-cell;
+  width: 40px;
+  height: 40px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+}
 </style>
